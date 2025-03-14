86e55f9448fef955a82cd20a4af2d6e6d41a2845
A Anand <a.anand.91119@gmail.com>
2025-03-14 08:17:51 +0530

    Adding base package

--- src/main/java/dev/notyouraverage/project/one/grpc/aggregator/grpc_stats_aggregator/GrpcStatsAggregatorApplication.java
+++ src/main/java/dev/notyouraverage/project/one/grpc/aggregator/grpc_stats_aggregator/GrpcStatsAggregatorApplication.java
@@ -2,8 +2,10 @@ package dev.notyouraverage.project.one.grpc.aggregator.grpc_stats_aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "dev.notyouraverage.project.base", "dev.notyouraverage.project.one" })
 public class GrpcStatsAggregatorApplication {
 
     public static void main(String[] args) {
